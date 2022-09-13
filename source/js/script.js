da96d22410a1f9411e3da19af865692c2649bb30
Liza Nemzer <zernemzer@gmail.com>
2022-09-13 15:57:19 +0300

    -

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,5 @@
 let navMain = document.querySelector(".main-navigation");
 let navToggle = document.querySelector(".main-navigation__toggle");
-let welcomeImg = document.querySelector(".welcome");
 
 
 navMain.classList.remove("main-navigation--nojs");
@@ -8,6 +7,4 @@ navMain.classList.remove("main-navigation--nojs");
 navToggle.addEventListener("click", function () {
   navMain.classList.toggle("main-navigation--closed");
   navMain.classList.toggle("main-navigation--opened");
-  welcomeImg.classList.toggle("welcome--closed");
-  welcomeImg.classList.toggle("welcome--opened");
 });
